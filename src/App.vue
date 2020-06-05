1557e0de8f93bca363e1460d8dea4da833f5b5b5
Tokyo_TG <tboynonilekan@gmail.com>
2020-06-05 16:21:49 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <dynamic-component/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import dynamicComponent from './components/dynamic-component.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    dynamicComponent
   }
 }
 </script>
---
+++ src/components/dynamic-component.vue
@@ -0,0 +1,52 @@
+<template>
+    <div :class="[expanded ? 'expanded' : 'closed']">
+        <slot></slot>
+    <button id="ex-btn" @click="expandComponent">Expand</button>
+    <button id="cl-btn" @click="closeComponent">Close</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"dynamicComponent",
+     data: function () {
+    return {
+      count: 0,
+      expanded:false,
+    }  
+  },
+  props:{
+      position:{
+          type:String,
+          default: "left"
+      },
+      size:{
+        type:String,
+          default: "400px"
+      }
+  },
+  methods: {
+    expandComponent(){
+        let exbtn = document.getElementById("ex-btn")
+        let clbtn = document.getElementById("cl-btn");
+                this.expanded = true;
+                exbtn.style.display = "none";
+                clbtn.style.display = "unset";
+    },
+    closeComponent(){
+        let exbtn = document.getElementById("ex-btn");
+        let clbtn = document.getElementById("cl-btn");
+        this.expanded = false;
+        clbtn.style.display = "none";
+        exbtn.style.display = "unset";
+      
+    }
+  },
+}
+</script>
+
+<style lang="css" scoped>
+    #cl-btn{
+    display: none;
+}
+
+</style>
